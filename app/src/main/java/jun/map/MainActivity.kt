974cfeec437d4c7470721b1ced552845b0d845c2
khj68 <maga40@naver.com>
2020-08-24 20:49:39 +0900

    Add library

--- app/src/main/java/jun/map/MainActivity.kt
+++ app/src/main/java/jun/map/MainActivity.kt
@@ -2,10 +2,19 @@ package jun.map
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.naver.maps.map.MapFragment
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMapSdk
+import com.naver.maps.map.indoor.IndoorRegion
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        NaverMapSdk.getInstance(this).client =
+            NaverMapSdk.NaverCloudPlatformClient("8nlf9bvios")
+        IndoorRegion indoorRegion = new IndoorRegion
+
     }
 }
